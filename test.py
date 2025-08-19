cee0d569cbe0b029db16e731fa55ac60844b8f86
Administrator <405246797@qq.com>
2025-08-19 22:11:37 +0800

    all new4

--- test.py
+++ test.py
@@ -3,17 +3,18 @@ import streamlit as st
 import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
-
+#import shap
+#shap.initjs()
 # 用户输入数据
 def user_input_features():
-    S1 = st.sidebar.slider('S1', 0, 300, 102)
-    S2 = st.sidebar.slider('S2', 0.00, 10.00, 4.86)
-    S3 = st.sidebar.slider('S3', 0.00, 50.00, 4.07)
-    S4 = st.sidebar.slider('S4', 0.00, 10.00, 0.45)
-    S5 = st.sidebar.slider('S5', 0.0, 10.0, 4.1)
-    S6 = st.sidebar.slider('S6', 0, 80, 53)
-    S7 = st.sidebar.slider('S7', 0.0, 90.0, 9.0)
-    S8 = st.sidebar.slider('S8', 0.0, 1000.0, 285.7)
+    S1 = st.sidebar.slider('2_B', 0, 300, 101)
+    S2 = st.sidebar.slider('3_C', 0.00, 10.00, 4.86)
+    S3 = st.sidebar.slider('8_H', 0.00, 50.00, 4.07)
+    S4 = st.sidebar.slider('19_S', 0.00, 10.00, 0.45)
+    S5 = st.sidebar.slider('25_Y', 0.0, 10.0, 4.1)
+    S6 = st.sidebar.slider('28_AB', 0, 80, 53)
+    S7 = st.sidebar.slider('30_AD', 0.0, 90.0, 9.0)
+    S8 = st.sidebar.slider('31_AE', 0.0, 1000.0, 285.7)
     data = {'2_B': S1, '3_C': S2,
             '8_H': S3, '19_S': S4,
             '25_Y': S5, '28_AB': S6,
@@ -26,7 +27,13 @@ df = user_input_features()
 model=joblib.load('AdaBoost.pkl')
 
 
+
+
 # 对输入数据进行分类，并进行展示
 prediction = model.predict(df)
 prediction_proba = model.predict_proba(df)
 st.write(f"预测结果: {prediction[0]}")
+
+
+#explainer = shap.TreeExplainer(model)
+#shap_values = explainer.shap_values(pd.DataFrame(df, columns=df.keys()))
