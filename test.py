7ebead22afef8a2a8d00fb345d9ae00728dac19a
Administrator <405246797@qq.com>
2025-08-21 00:58:51 +0800

    2025082102

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import joblib
 import streamlit as st
 import pandas as pd
-
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
 #import shap
 #shap.initjs()
 # 用户输入数据
